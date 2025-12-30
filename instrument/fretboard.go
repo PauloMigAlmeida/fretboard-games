bc191a024d81b5ec965c641267a3c8850431fa72
PauloMigAlmeida <paulo.miguel.almeida.rodenas@gmail.com>
2025-12-31 09:56:41 +1300

    implement drawing fretboard

--- instrument/fretboard.go
+++ instrument/fretboard.go
@@ -3,6 +3,8 @@ package instrument
 import (
 	"fmt"
 	"github.com/PauloMigAlmeida/fretboard-games/music"
+	"slices"
+	"strings"
 )
 
 type Fretboard struct {
@@ -23,7 +25,7 @@ func NewFretboard(numOfFrets int, tuning []*music.Note) *Fretboard {
 
 func (f *Fretboard) GetNoteAt(stringNumber int, fretNumber int) (*music.Note, error) {
 	if stringNumber < 1 {
-		return nil, fmt.Errorf("strings are 1-indexed to be relation to how we number them in real world")
+		return nil, fmt.Errorf("strings are 1-indexed to be like how we number them in real world")
 	}
 
 	if stringNumber > len(f.Strings) {
@@ -36,4 +38,46 @@ func (f *Fretboard) GetNoteAt(stringNumber int, fretNumber int) (*music.Note, er
 
 	// strings are 1-indexed
 	return &f.Strings[stringNumber-1].FretNotes[fretNumber], nil
+}
+
+func (f *Fretboard) DrawFretboard(notes []*music.Note, ignoreStrings []int) string {
+	var sb strings.Builder
+
+	// Header
+	sb.WriteString("|")
+	for idx := range f.Strings[0].FretNotes {
+		sb.WriteString(fmt.Sprintf(" %-3d|", idx))
+	}
+	sb.WriteString("\n")
+
+	// Body
+	for strIdx, strEl := range f.Strings {
+		sb.WriteString("|")
+		for _, note := range strEl.FretNotes {
+
+			if slices.Contains(ignoreStrings, strIdx) {
+				sb.WriteString(fmt.Sprintf(" %-3s", "-"))
+			} else {
+				found := false
+				for _, n := range notes {
+					if n.Equals(&note) {
+						sb.WriteString(fmt.Sprintf(" %-3s", "X"))
+						found = true
+					}
+				}
+
+				if !found {
+					sb.WriteString(fmt.Sprintf(" %-3s", "-"))
+				}
+			}
+
+			sb.WriteString("|")
+		}
+
+		if strIdx < len(f.Strings)-1 {
+			sb.WriteString("\n")
+		}
+	}
+
+	return sb.String()
 }
--- instrument/fretboard_test.go
+++ instrument/fretboard_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/PauloMigAlmeida/fretboard-games/music"
 	"github.com/stretchr/testify/assert"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -53,4 +54,57 @@ func TestFretboard_GetNoteAt(t *testing.T) {
 	note, err = fretboard.GetNoteAt(1, 25)
 	assert.Nil(t, note)
 	assert.Error(t, err)
+}
+
+func TestFretboard_DrawFretboard(t *testing.T) {
+	fretboard := NewFretboard(12, StandardTuning())
+
+	noteA, _ := music.FindNote(music.A, music.Natural)
+	noteC, _ := music.FindNote(music.C, music.Natural)
+
+	// Without ignoring strings
+	ret := fretboard.DrawFretboard([]*music.Note{
+		noteA,
+	}, []int{})
+
+	assert.Equal(t, strings.TrimSpace(`
+| 0  | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  | 9  | 10 | 11 |
+| -  | -  | -  | -  | -  | X  | -  | -  | -  | -  | -  | -  |
+| -  | -  | -  | -  | -  | -  | -  | -  | -  | -  | X  | -  |
+| -  | -  | X  | -  | -  | -  | -  | -  | -  | -  | -  | -  |
+| -  | -  | -  | -  | -  | -  | -  | X  | -  | -  | -  | -  |
+| X  | -  | -  | -  | -  | -  | -  | -  | -  | -  | -  | -  |
+| -  | -  | -  | -  | -  | X  | -  | -  | -  | -  | -  | -  |
+`), ret)
+
+	ret = fretboard.DrawFretboard([]*music.Note{
+		noteC,
+	}, []int{})
+
+	assert.Equal(t, strings.TrimSpace(`
+| 0  | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  | 9  | 10 | 11 |
+| -  | -  | -  | -  | -  | -  | -  | -  | X  | -  | -  | -  |
+| -  | X  | -  | -  | -  | -  | -  | -  | -  | -  | -  | -  |
+| -  | -  | -  | -  | -  | X  | -  | -  | -  | -  | -  | -  |
+| -  | -  | -  | -  | -  | -  | -  | -  | -  | -  | X  | -  |
+| -  | -  | -  | X  | -  | -  | -  | -  | -  | -  | -  | -  |
+| -  | -  | -  | -  | -  | -  | -  | -  | X  | -  | -  | -  |
+`), ret)
+
+	// ignoring strings
+	ret = fretboard.DrawFretboard([]*music.Note{
+		noteA,
+	}, []int{
+		6, 5, 4, 3, 2,
+	})
+
+	assert.Equal(t, strings.TrimSpace(`
+| 0  | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  | 9  | 10 | 11 |
+| -  | -  | -  | -  | -  | X  | -  | -  | -  | -  | -  | -  |
+| -  | -  | -  | -  | -  | -  | -  | -  | -  | -  | X  | -  |
+| -  | -  | -  | -  | -  | -  | -  | -  | -  | -  | -  | -  |
+| -  | -  | -  | -  | -  | -  | -  | -  | -  | -  | -  | -  |
+| -  | -  | -  | -  | -  | -  | -  | -  | -  | -  | -  | -  |
+| -  | -  | -  | -  | -  | -  | -  | -  | -  | -  | -  | -  |
+`), ret)
 }
